dd8ee9d9157c1f1f0b0715e4a8fe0de734496b5e
Mandeep-Std <mandeep-std@Mandeeps-MBP.fios-router.home>
2018-11-29 18:47:04 -0500

    AWS CodeDeploy Configuration

---
+++ scripts/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo yum update -y
+sudo yum install -y docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+newgrp docker
+sudo usermod -a -G docker ec2-user
+chown -R ec2-user:ec2-user /artemis
---
+++ scripts/start_application.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /artemis/cicd_test
+docker-compose up --build -d > /dev/null
