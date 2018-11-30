35d0e348fccddd2fab378b874d8a01aa55e5a233
Mandeep-Std <mandeep-std@Mandeeps-MBP.fios-router.home>
2018-11-29 22:39:57 -0500

    AWS CodeDeploy Configuration

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,8 +2,8 @@
 
 sudo yum update -y
 sudo yum install -y docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
-sudo chmod +x /opt/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 newgrp docker
 sudo usermod -a -G docker ec2-user
 chown -R ec2-user:ec2-user /artemis
